80c77f6cca672ef163ad1cae671147802d45bcdc
hansdeep <hansdeep.singh@hotmail.com>
2022-02-03 09:55:48 +1100

    Podcast page added, Navigation fixed

--- src/app/podcasts/podcasts.component.ts
+++ src/app/podcasts/podcasts.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'matt-podcasts',
   templateUrl: './podcasts.component.html',
-  styleUrls: ['./podcasts.component.scss']
+  styleUrls: ['./podcasts.component.scss'],
 })
 export class PodcastsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() count = 0;
+  podcasts: podcasts[] | undefined;
+  podcastsUrl: podcasts[] | undefined;
+
+  urlbefore: string = "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FOsloHealthandFitnessFTG%2Fvideos%";
+  urlafter: string = "%2F&show_text=false&width=560&t=0";
+  
 
   ngOnInit(): void {
-  }
+    this.podcasts = [
+      {
+        url: "2F322500316092170",
+        desc: "Weekly podcast series Episode 4! ⭐ We are back after a 3-week lockdown! This week we talk about the importance of mindset and routine and how to ensure we continue to push ourselves in the right direction after a lockdown. If you're feeling like your training has lost momentum after the lockdown then make sure you take a listen! Any questions as per usual team, please leave them in the comment section below! Have a great Sunday night! 🥰",
+      },
+      {
+        url: '2F2961775330771418',
+        desc: 'Mistake 4 Lack of Accountability Day 4, Video 4 of our 5 day video series where we take a look at the top five mistakes that even the most health conscious people make that could be stopping them from achieving the body of their dreams! Today we look at what affects the  decision making process of our food intake! If you found value in today s video, you can show your appreciation by smashing the like button and if you like to receive more content like this, you can subscribe for more great content',
+      },
+      
+    ];
 
+
+    this.podcasts = this.podcasts.map(el=>({url:this.urlbefore+el.url+this.urlafter,desc:el.desc}))
+
+    this.podcastsUrl =
+      this.count != 0 ? this.podcasts.slice(0, this.count) : this.podcasts;
+  }
+}
+interface podcasts {
+  url: string;
+  desc: string;
 }
--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -15,34 +15,36 @@ export class VideosComponent implements OnInit {
   ngOnInit(): void {
     this.videos = [
       {
-        url: 'https://www.youtube.com/embed/oMilfXjvuAs',
-        desc: 'Mistake 4 Lack of Accountability Day 4, Video 4 of our 5 day video series where we take a look at the top five mistakes that even the most health conscious people make that could be stopping them from achieving the body of their dreams! Today we look at what affects the  decision making process of our food intake! If you found value in today s video, you can show your appreciation by smashing the like button and if you like to receive more content like this, you can subscribe for more great content',
+        url: 'oMilfXjvuAs',
+        desc: "Mistake 4 Lack of Accountability Day 4, Video 4 of our 5 day video series where we take a look at the top five mistakes that even the most health conscious people make that could be stopping them from achieving the body of their dreams! Today we look at what affects the  decision making process of our food intake! If you found value in today s video, you can show your appreciation by smashing the like button and if you like to receive more content like this, you can subscribe for more great content",
       },
       {
-        url: 'https://www.youtube.com/embed/Sb58EmNs5O8',
+        url: 'Sb58EmNs5O8',
         desc: 'Day 3, Video 3 of our 5 day video series looking at the Top 5 mistakes even the most health conscious people make that could be stopping them from achieving the body of their dreams!    If you enjoyed this video, please share your appreciation by smashing the 👍 button and subscribing below',
       },
       {
-        url: 'https://www.youtube.com/embed/h6tg19K8cq4',
+        url: 'h6tg19K8cq4',
         desc: 'Day 2, Video 2 of our 5 day video series Top 5 Mistakes health conscious people make in their food intake that prevent them from achieving great results!',
       },
       {
-        url: 'https://www.youtube.com/embed/5vNumpupXfM',
+        url: '5vNumpupXfM',
         desc: 'In our 5 Day, 5 Video series we look at the top 5 mistakes that most health conscious people make, that prevent them from getting great results! Day One: Mistake One Does Calorie Counting really work? Should you be doing it? We look forward to your comments below, If you like this video please subscribe and give us a thumbs up👍',
       },
       {
-        url: 'https://www.youtube.com/embed/Sb58EmNs5O8',
+        url: 'Sb58EmNs5O8',
         desc: 'Day 3, Video 3 of our 5 day video series looking at the Top 5 mistakes even the most health conscious people make that could be stopping them from achieving the body of their dreams!    If you enjoyed this video, please share your appreciation by smashing the 👍 button and subscribing below',
       },
       {
-        url: 'https://www.youtube.com/embed/h6tg19K8cq4',
+        url: 'h6tg19K8cq4',
         desc: 'Day 2, Video 2 of our 5 day video series Top 5 Mistakes health conscious people make in their food intake that prevent them from achieving great results!',
       },
       {
-        url: 'https://www.youtube.com/embed/5vNumpupXfM',
+        url: '5vNumpupXfM',
         desc: 'In our 5 Day, 5 Video series we look at the top 5 mistakes that most health conscious people make, that prevent them from getting great results! Day One: Mistake One Does Calorie Counting really work? Should you be doing it? We look forward to your comments below, If you like this video please subscribe and give us a thumbs up👍',
       },
     ];
+
+    this.videos = this.videos.map(el=>({url:this.url+el.url, desc:el.desc}))
 
     this.videosUrl =
       this.count != 0 ? this.videos.slice(0, this.count) : this.videos;
